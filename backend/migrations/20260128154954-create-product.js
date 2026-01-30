372cf4b27e1052e7a275223966804d3b1b738f74
achchuthan <achchuthany@univ.jfn.ac.lk>
2026-01-30 09:41:49 +0530

    Update product migration file

--- backend/migrations/20260128154954-create-product.js
+++ backend/migrations/20260128154954-create-product.js
@@ -1,36 +1,34 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("products", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING(30),
-      },
-      price: {
-        type: Sequelize.FLOAT,
-      },
-      type: {
-        type: Sequelize.STRING(30),
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        field: "created_at",
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        field: "updated_at",
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("products");
-  },
-};
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable("products", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      type: Sequelize.STRING(30),
+    },
+    price: {
+      type: Sequelize.FLOAT,
+    },
+    type: {
+      type: Sequelize.STRING(30),
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      field: "created_at",
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      field: "updated_at",
+    },
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable("products");
+}
